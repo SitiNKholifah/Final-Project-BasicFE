9f2ed41e193e558bb1dddb4a6edb05b5291d6104
Siti Nur Kholifah <lilyy.sitink@gmail.com>
2023-05-23 16:25:40 +0700

    first commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Siti Nur Kholifah'; // TODO: replace this
+
+const studentId = 'FE5174358'; // TODO: replace this
+
+const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
